cd306ef50bf1e9b7216c23fb015c91475933f83a
Niko Ugarov <xxkkoo@gmail.com>
2021-11-03 16:30:02 +0100

    #7: generic influx writer

--- src/main/scala/filesfu/collector/Influx.scala
+++ src/main/scala/filesfu/collector/Influx.scala
@@ -2,23 +2,21 @@ package filesfu.collector
 
 import akka.NotUsed
 import akka.stream._
-import akka.stream.alpakka.influxdb.{InfluxDbWriteMessage, InfluxDbWriteResult}
 import akka.stream.alpakka.influxdb.scaladsl.InfluxDbFlow
+import akka.stream.alpakka.influxdb.{InfluxDbWriteMessage, InfluxDbWriteResult}
 import akka.stream.scaladsl._
 import org.influxdb.InfluxDB
 import org.influxdb.dto.Point
 
 object Influx  {
 
-  type Msg = InfluxDbWriteMessage[Point, NotUsed]
+  def writer[T](perGroup: Int = 5, parGroups:Int = 20)(implicit db:InfluxDB, marshaller: Flow[T,Point,_]) =
+    Flow[T] via marshaller via pointWriter(perGroup, parGroups)
 
-  def pointSink(perGroup: Int = 5, parGroups:Int = 20)(implicit db:InfluxDB) =
-    pointFlow(perGroup,parGroups).toMat(Sink.ignore)(Keep.right)
+  def pointWriter(perGroup: Int = 5, parGroups:Int = 20)(implicit db:InfluxDB): Flow[Point, InfluxDbWriteResult[Point, NotUsed], NotUsed] =
+    Flow[Point] map InfluxDbWriteMessage.apply via grouper(InfluxDbFlow.create(), perGroup, parGroups) mapConcat identity
 
-  def pointFlow(perGroup: Int = 5, parGroups:Int = 20)(implicit db:InfluxDB): Flow[Point, Seq[InfluxDbWriteResult[Point, NotUsed]], NotUsed] =
-    Flow[Point] map InfluxDbWriteMessage.apply via groupedFlow(InfluxDbFlow.create(), perGroup, parGroups)
-
-  def groupedFlow[In,Out](worker: Flow[Seq[In],Out, NotUsed], perGroup: Int = 5, parGroups:Int = 20): Flow[In, Out, NotUsed] = {
+  private def grouper[In,Out](worker: Flow[Seq[In],Out, NotUsed], perGroup: Int = 5, parGroups:Int = 20): Flow[In, Out, NotUsed] = {
     import GraphDSL.Implicits._
     Flow[In].grouped(perGroup).via( GraphDSL.create() { implicit b =>
       val balancer = b.add(Balance[Seq[In]](parGroups))
