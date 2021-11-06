810741f4ea3678b3b0551f16e36958149d0dbfc6
Niko Ugarov <xxkkoo@gmail.com>
2021-11-06 20:29:37 +0100

    #22: smarter client simulation - can now demo some cases

--- src/it/scala/filesfu/collector/InfluxIntegration.scala
+++ src/it/scala/filesfu/collector/InfluxIntegration.scala
@@ -1,53 +1,86 @@
 package filesfu.collector
 
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
-import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalacheck.Gen.choose
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
+import akka.testkit.TestDuration
+import filesfu.collector.protocol.SessionState
+import org.scalacheck.Gen._
 import org.scalacheck.{Gen, Shrink}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import akka.http.scaladsl.testkit.RouteTestTimeout
-import akka.testkit.TestDuration
+
+import java.time.Instant
+import java.util.UUID
 import scala.concurrent.duration._
 
 
-class InfluxIntegration extends AnyWordSpec with Matchers with ScalatestRouteTest with ScalaCheckPropertyChecks {
+class InfluxIntegration extends AnyWordSpec with Matchers with ScalatestRouteTest with ScalaCheckPropertyChecks
+  with SessionGenerators {
+
   implicit def noShrink[T]: Shrink[T] = Shrink.shrinkAny
   // https://gist.github.com/davidallsopp/f65d73fea8b5e5165fc3#gistcomment-2339650
 
   implicit val timeout = RouteTestTimeout(10.seconds.dilated)
 
-  "streams/sessions" can {
-    val times = Iterator.iterate(System.currentTimeMillis())(_ - 1000)
-
-    def postLines(lines: String*) =
-      Post("/streams/sessions").withEntity(
-        HttpEntity(ContentTypes.`application/json`, lines.mkString("\n"))
-      ) ~> Server.routes ~> check {
-        status shouldBe StatusCodes.OK
-        responseAs[String] shouldBe s"{\"msg\":\"Total messages received: ${lines.size}\"}"
-      }
-
-    "write some sessions with cpu peaks " in {
-      val cpustats = Gen.frequency(80 -> choose(.05, .25),
-                                   40 -> choose(.25, .30),
-                                   20 -> choose(.30, .40),
-                                   10 -> choose(.40, .50),
-                                   5  -> choose(.85, .88),
-                                   10 -> choose(.88, .95))
-      val users = Seq("Harry", "Larry", "Sally", "Molly")
-      val sessions = users.map(u => s"${u}s-Session")
-      val version = "604"
-      sessions.foreach { s =>
-        postLines(s"{\"sessionID\":\"$s\", \"version\": \"$version\", \"state\":\"start\", \"timestamp\": ${times.next()} }")
-      }
-
-      forAll(Gen.listOfN(15,cpustats), Gen.oneOf(sessions), MinSuccessful(100)) { (cpus, session) =>
-        val lines = cpus.map(cpu => s"{\"timestamp\":${times.next()},\"sessionID\":\"$session\", \"cpu\":$cpu }")
-        postLines(lines:_*)
-      }
+  def postLines(path: String, lines: String*) = {
+    println(lines.mkString("\n"))
+    Post(path).withEntity(HttpEntity(ContentTypes.`application/json`, lines.mkString("\n"))) ~>
+      Server.routes ~> check {
+      status shouldBe StatusCodes.OK
+      responseAs[String] shouldBe s"{\"msg\":\"Total messages received: ${lines.size}\"}"
     }
+  }
+
+  def postCPU(path: String, sid: String, cpus: (Double, EpochMs)*) = {
+      val lines = cpus.map { case (cpu, time) =>
+        s"{\"timestamp\":$time,\"sessionID\":\"$sid\", \"cpu\":$cpu }"
+      }
+      postLines(path, lines: _*)
+    }
+
+
+  "client" can {
+    val Path = "/streams/sessions"
+    import SessionState._
+
+    def postSession(user: String, version: String, sid: UUID, phase: Map[protocol.SessionState.Value, Phase]) = {
+      postLines(Path, s"{\"sessionID\":\"$sid\", \"version\": \"$version\", \"state\":\"start\", \"timestamp\": ${phase(start).startAt} }")
+      postCPU(Path, sid.toString, phase(start).cpus: _*)
+      postLines(Path, s"{\"sessionID\":\"$sid\", \"userID\": \"$user\", \"state\":\"login\", \"timestamp\": ${phase(login).startAt} }")
+      postCPU(Path, sid.toString, phase(login).cpus: _*)
+      postLines(Path, s"{\"sessionID\":\"$sid\", \"state\":\"running\", \"timestamp\": ${phase(running).startAt} }")
+      postCPU(Path, sid.toString, phase(running).cpus: _*)
+      postLines(Path, s"{\"sessionID\":\"$sid\", \"state\":\"shutdown\", \"timestamp\": ${phase(shutdown).startAt} }")
+      postCPU(Path, sid.toString, phase(shutdown).cpus: _*)
+      postLines(Path, s"{\"sessionID\":\"$sid\", \"state\":\"over\", \"timestamp\": ${phase(over).startAt} }")
+    }
+
+    "post sessions with high CPU at login" ignore {
+      val versions = Gen.oneOf(Seq("700"))
+      val phases = phaseGen(System.currentTimeMillis() - 3600000,
+                            (start, normal, 2.seconds),
+                            (login, highWithSurges, 4.seconds),
+                            (running, higher(normal), 10.seconds),
+                            (shutdown, normal, 5.seconds),
+                            (over, normal, 0.seconds)
+                            )
+      forAll(users, versions, Gen.uuid, phases, minSuccessful(5))(postSession)
+    }
+
+    "post normal sessions" in {
+      val versions = Gen.oneOf(Seq("604","605"))
+      val phases = phaseGen(System.currentTimeMillis() - 3600000,
+                            (start, normal, 2.seconds),
+                            (login, normal, 4.seconds),
+                            (running, normal, 20.seconds),
+                            (shutdown, normal, 5.seconds),
+                            (over, normal, 0.seconds)
+                            )
+      forAll(users, versions, Gen.uuid, phases, minSuccessful(5))(postSession)
+    }
+
+
   }
 
 }
